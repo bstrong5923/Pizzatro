74e4cd48973baf85e86e5fc50aa65809e7bc1547
bstrong5923 <bstrong5923@student.pps.net>
2025-10-07 14:22:02 -0700

    got the cards clickable

--- Scripts/card.gd
+++ Scripts/card.gd
@@ -1,13 +1,17 @@
 extends Sprite2D
 
-var logo = $ingredient_logo
+
 
 func _ready():
+	var logo = $ingredient_logo
 	# random ingredient logo
 	var frames = logo.texture.get_width() / logo.region_rect.size.x
 	var random_index = randi_range(0, frames - 1)
 	logo.set_ingredient(random_index)
 
-func _input(event):
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
-		logo.set_ingredient(4)
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if is_pixel_opaque(get_local_mouse_position()): # was I the thing that was clicked?
+			var logo = $ingredient_logo
+			var random_index = randi_range(0, 5)
+			logo.set_ingredient(random_index)
