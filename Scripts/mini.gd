1f1d52e21b71d9bdcde89bd57be7046482651fc9
bstrong5923 <bstrong5923@student.pps.net>
2025-10-07 15:33:56 -0700

    workin on cloning the minis but its broken

--- Scripts/mini.gd
+++ Scripts/mini.gd
@@ -1,8 +1,11 @@
 extends Sprite2D
 
+var is_a_clone = false
+func dont_clone():
+	is_a_clone = true
+
 func scatter(ingredient):
 	texture = load("res://Assets/Sprites/ingredient_minis/" + str(ingredient) + ".png") # get the correct spritesheet
-	for m in range(0, randi_range(4,6)): # create between 4 and 6 minis
 		region_rect.position.x = randi_range(0,4) * 5 # random mini of the ingredient
 		# random position in the pizza
 		var x = 200
@@ -14,6 +17,23 @@ func scatter(ingredient):
 		position = Vector2(x, y)
 		# random rotation
 		rotation_degrees = randi_range(0,3) * 90
+		# clone it
 		
+		
+		#var newnode = Sprite2D.new()
+		#newnode.texture = texture
+		#newnode.position = position
+		#var instance = newnode.instantiate()
+		#addchild(instance)
+		print("cloned.")
+		
+func create_clones(ingredient):
+	for m in range(0, randi_range(4,7)): # create between 4 and 6 minis
+		var mynode = preload("res://Scenes/mini.tscn")
+		var instance = mynode.instantiate()
+		instance.scatter(ingredient)
+		add_child(instance)
+
 func _ready() -> void:
-	scatter(2)
+	if is_a_clone:
+		create_clones(3)
