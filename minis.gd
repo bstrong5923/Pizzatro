84f0ee4ad65fd125054308d8bcc69a27d97d4733
bstrong5923 <drizzeerascoll@gmail.com>
2025-10-08 13:54:58 -0700

    made the minis look better distributed

--- minis.gd
+++ minis.gd
@@ -5,22 +5,27 @@ func dont_clone():
 	original = false
 
 func scatter(ingredient):
-	for m in range(0, randi_range(4,7)): # create between 4 and 6 minis
+	var init_angle = randi_range(0,359)
+	var num_of_minis = randi_range(5,7)
+	for m in range(0, num_of_minis): # create between 4 and 6 minis
+		
 		# clone it
 		var mynode = preload("res://Scenes/minis.tscn")
 		var instance = mynode.instantiate()
+		
 		# get the correct spritesheet
 		instance.texture = load("res://Assets/Sprites/ingredient_minis/" + str(ingredient) + ".png")
-		# random position in the pizza
-		var x = 200
-		var y = 200
-		while pow(x, 2) + pow(y, 2) >= pow(102, 2): # while it is not in the pizza, try again
-			x = randi_range(-17, 18) * -6 - 3 # position is relative to current position
-			y = randi_range(-17, 18) * -6
-		instance.position = Vector2(x, y)
 		# random mini of the ingredient
-		instance.region_rect.position.x = randi_range(0,4) * 5 
-		# random rotation
+		instance.region_rect.position.x = randi_range(0,4) * 5
+		
+		# random position in the pizza
+		var angle = init_angle + (360 / num_of_minis) * m + randi_range(-5,5) # each one has an evenly spaced angle (0, 90, 180, 270 if there were four minis)
+		var radius = pow(randi_range(36, 10404), 0.5) # random radius from the middle of the pizza to the edge, further out is more common
+		var x = radius * cos(deg_to_rad(angle)) # get the x and y
+		var y = radius * sin(deg_to_rad(angle))
+		instance.position = Vector2(x, y)
+		 
+		# random rotation, then scale it and tell it it isn't the original
 		instance.rotation_degrees = randi_range(0,3) * 90
 		instance.scale.x = 6
 		instance.scale.y = 6
