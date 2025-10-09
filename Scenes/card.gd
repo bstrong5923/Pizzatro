5259a4a19cf458d5db78f2e884fb2ffda439d20a
bstrong5923 <bstrong5923@student.pps.net>
2025-10-09 15:12:06 -0700

    added hand animation and card cooldown

--- Scenes/card.gd
+++ Scenes/card.gd
@@ -2,19 +2,23 @@ extends Sprite2D
 
 var ingredient = 0
 
+var play_timer
+
 func _ready():
 	# random ingredient logo
 	var logo = $ingredient_logo
 	var frames = logo.texture.get_width() / logo.region_rect.size.x
 	var random_index = randi_range(0, frames - 1)
 	set_ingredient(random_index)
+	play_timer = get_node("/root/Game/card_timer")
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed and play_timer.can_play_a_card:
 		if is_pixel_opaque(get_local_mouse_position()): # was I the thing that was clicked?
-			# scatter the mini ingredients
-			var minis = get_node("/root/Game/minis")
-			minis.scatter(ingredient)
+			# animation to add the ingredients
+			var hand = get_node("/root/Game/hand_animation")
+			hand.go(ingredient)
+			play_timer.cooldown()
 
 func set_ingredient(i):
 	ingredient = i;
