6da8de35e528ccf9eb2078a2a5369fc04ba5e253
bstrong5923 <bstrong5923@student.pps.net>
2025-11-12 15:32:05 -0800

    pricetags call from json now

--- Scenes/card.gd
+++ Scenes/card.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var ingredient = 0
+var price = 0
 
 var play_timer = preload("res://Scenes/card_timer.gd")
 @onready
@@ -10,12 +11,12 @@ func _ready() -> void:
 	get_viewport().set_physics_object_picking_first_only(true)
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void: # on click
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed and play_timer.can_play_a_card and get_node("/root/Game/Labels/Balance").get_balance() > 0:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed and play_timer.can_play_a_card and get_node("/root/Game/Labels/Balance").get_balance() >= price:
 		var hand = get_node("/root/Game/hand_animation") # animation to add the ingredients
 		hand.go(ingredient)
 		play_timer.cooldown() # wait for animation to finish before you can click another
 		discard_self() # discard this one
-		get_node_or_null("/root/Game/Labels/Balance").change_balance(-3)
+		get_node_or_null("/root/Game/Labels/Balance").change_balance(-price)
 		var checker = get_node("card_function") # do the function
 		checker.set_card(ingredient)
 		checker.check_function()
@@ -24,12 +25,22 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 
 func set_ingredient(i):
 	ingredient = i
+	# get the price from the JSON
+	var file_path = "res://Assets/ingredient_prices.json"
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	if file:
+		var json_string = file.get_as_text()
+		var parsed_json = JSON.parse_string(json_string)
+		file.close()
+		if parsed_json != null:
+			price = int(parsed_json[ingredient])
+			
 	# get the right logo
 	var logo = $ingredient_logo
 	logo.set_ingredient(i)
 	# get the price
 	var pricetag = $price_circle
-	pricetag.set_price(3)
+	pricetag.set_price(price)
 	
 func get_ingredient():
 	return ingredient
