e7fcadec1060b0b47a2d93bde37a182349d18699
LucianLandey <llandey8161@student.pps.net>
2025-11-14 14:17:02 -0800

    added camera back thing

--- Scenes/Camera.gd
+++ Scenes/Camera.gd
@@ -8,3 +8,9 @@ func _on_go_to_shop_button_pressed() -> void:
 	var target_x := 230.0 
 	var tween := get_tree().create_tween()
 	tween.tween_property(self, "position:x", target_x, 1.0)
+
+
+func _on_go_to_game_button_pressed() -> void:
+	var target_x := 0.0 
+	var tween := get_tree().create_tween()
+	tween.tween_property(self, "position:x", target_x, 1.0)
