7adf748ca07e4ec432cf5194471aff6674859660
LucianLandey <llandey8161@student.pps.net>
2025-12-11 15:31:36 -0800

    added anim bobby joe
    Hi. Hi. anim on the shop cards. everything looks gorgeous

--- Scripts/card.gd
+++ Scripts/card.gd
@@ -33,7 +33,7 @@ func set_ingredient(i):
 	
 	# get the price
 	var pricetag = $price_circle
-	pricetag.set_price(price)
+	pricetag.set_price(price, true)
 	
 func get_ingredient():
 	return ingredient
@@ -51,7 +51,7 @@ func _on_area_2d_mouse_exited():
 		change_scale(1)
 	
 func change_scale(n):
-	$price_circle.set_size(n)
+	$price_circle.set_size(n, true)
 	$ingredient_logo.scale = Vector2(5 * n, 5 * n)
 	$blank.scale = Vector2(5 * n, 5 * n)
 	$Area2D.scale = Vector2(5 * n, 5 * n)
--- Scripts/price_circle.gd
+++ Scripts/price_circle.gd
@@ -2,21 +2,21 @@ extends Node2D
 
 var price
 var label = Label.new()
-
-func set_price(p):
+#defaultpos should be only be false if you dont want the price circle to be at the top right of the card
+func set_price(p, defaultpos):
 	price = p
-	display_price()
+	display_price(defaultpos)
 
-func display_price():
+func display_price(defaultpos):
 	label.text = "$" + str(price)
 	add_child(label)
 	# wait until the label has been loaded
 	await label.ready
 	await get_tree().process_frame
 	# scale it right
-	set_size(1)
+	set_size(1, defaultpos)
 
-func set_size(n):
+func set_size(n, defaultpos):
 	# get the right label font
 	label.add_theme_font_size_override("font_size", 20 * n)
 	label.add_theme_color_override("color", Color(10,0,0))
@@ -25,7 +25,8 @@ func set_size(n):
 	label.add_theme_font_override("font", f)
 	# scale the circle up
 	$circle.scale = Vector2(5 * n, 5 * n)
-	position = Vector2(11 * 5 * n, -20 * 5 * n) # (11, -20) are the default coordinates
+	if defaultpos == true:
+		position = Vector2(11 * 5 * n, -20 * 5 * n) # (11, -20) are the default coordinates
 	# Resets size to fit text + font
 	label.reset_size()
 	label.queue_redraw()
@@ -34,4 +35,6 @@ func set_size(n):
 	label.position.x += n * 3
 	label.position.y += n * 3
 
-	
+#doesnt work unless you have defaultpos be passed as false
+func change_pos(x, y):
+	position = Vector2(x, y)
