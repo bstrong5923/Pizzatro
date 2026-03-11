836915edf1e02c8a335ca4b00b7a4b407d4c7f75
bstrong5923 <bstrong5923@student.pps.net>
2026-03-11 15:33:07 -0700

    added cleannum and workin on score numbering system

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,3 +3,13 @@ extends Node
 
 func _ready() -> void:
 	var game_scene = game.instantiate()
+
+func cleannum(v):
+	var result
+	if v == float(int(v)) or v >= 1000: # 3.0 --> 3,   1000.5 --> 1000
+		result = int(v)
+	elif v < 100: # 33.3333333 --> 33.33
+		result = snapped(v, 0.01)
+	else:  # 333.3333333 --> 333.3
+		result = snapped(v, 0.1)
+	return result
