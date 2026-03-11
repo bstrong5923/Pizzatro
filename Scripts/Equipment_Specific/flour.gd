3a08fbf3dab1cf11989bee352512ba09f52bfe4e
imoore7128 <isaacmoore3325@gmail.com>
2026-03-11 15:11:29 -0700

    Hello Mr Mandell, I didnt add anything into our godot code but I started and nearly finished the final version of our shop screen background today
    asda

--- Scripts/Equipment_Specific/flour.gd
+++ Scripts/Equipment_Specific/flour.gd
@@ -1,6 +1,8 @@
 extends Equipment
 class_name Flour
 
-static func get_multiplier(flour_count: int) -> float:
+func get_multiplier(flour_count: int) -> float:
 	# 1 Flour = 1.1x, 2 Flour = 1.2x, etc.
 	return 1.0 + (float(flour_count) / 10.0)
+
+#yahoo (adding this to e able to make a commit)
