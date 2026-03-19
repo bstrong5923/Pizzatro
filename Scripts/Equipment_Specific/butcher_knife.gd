c6d535b11a5f3e2b60ad7e5c9946e38a9dc99736
LucianLandey <llandey8161@student.pps.net>
2026-03-19 14:14:15 -0700

    fixed butcher knife

--- Scripts/Equipment_Specific/butcher_knife.gd
+++ Scripts/Equipment_Specific/butcher_knife.gd
@@ -5,11 +5,11 @@ var multiplier = 2.5
 
 func on_card_played(ingredient):
 	if ingredient.types.has("meat"):
-#		for f in CardFunction.flavors_to_add.size():
-#			CardFunction.flavors_to_add[f] *= multiplier
-	#	for i in range(ingredient.flavors.size()):
+		for f in CardFunction.flavors_to_add.size():
+			CardFunction.flavors_to_add[f] *= multiplier
+		#for i in range(ingredient.flavors.size()):
 		#	ingredient.flavors[i] *= multiplier
-			var new_flavors = ingredient.flavors.duplicate()
-			for i in range(new_flavors.size()):
-				new_flavors[i] *= multiplier
-			ingredient.flavors = new_flavors
+	#		var new_flavors = ingredient.flavors.duplicate()
+	#		for i in range(new_flavors.size()):
+		#		new_flavors[i] *= multiplier
+	#		ingredient.flavors = new_flavors
