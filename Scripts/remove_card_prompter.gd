a7e328e8b7d7c5e29e9d38555452a220faaa1ac3
LucianLandey <llandey8161@student.pps.net>
2026-04-07 14:28:07 -0700

    Added price circle to shop pack and shop remove card functions

--- Scripts/remove_card_prompter.gd
+++ Scripts/remove_card_prompter.gd
@@ -3,6 +3,11 @@ extends Sprite2D
 const REMOVE_BUTTON_PRESSED_TEXTURE := preload("res://Assets/Sprites/game scene items/remove_button_pressed.png")
 const REMOVE_BUTTON_TEXTURE := preload("res://Assets/Sprites/game scene items/remove_button.png")
 
+func _ready():
+	var pricetag = get_node("price_circle")
+	pricetag.set_price(25, true)
+	pricetag.equipment_set_size(1)
+
 func _on_area_2d_input_event(_viewport, event, _shape_idx):
 	if (event is InputEventMouseButton \
 	and event.button_index == MOUSE_BUTTON_LEFT \
