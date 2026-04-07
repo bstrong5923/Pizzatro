a3b12ec4fefdc08fa1a643f42f00f5895abae531
LucianLandey <llandey8161@student.pps.net>
2026-04-07 14:51:40 -0700

    added start of minimum system
    easy to scale, primative minimum system. after set rounds just multiplies by 2

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,18 +1,18 @@
 extends Node2D
 
 var level 
-var minimums = [12, 20, 30, 42, 65, 80]
+var minimums = [12, 20, 30, 42, 65, 80, 100]
 var playing = false
 var starting_energy
-
+var n = 1
 func _ready() -> void:
 	level = 0
 	Deck.fill_initial_deck()
 	new_round()
+	n = 1
 
 func new_round():
 	level += 1
-	
 	starting_energy = 20
 	# check equipments
 	for e in Equip.get_my_equipment():
@@ -22,7 +22,9 @@ func new_round():
 	if level <= minimums.size():
 		Deck.set_minimum(minimums[level - 1])
 	else:
-		Deck.set_minimum(100)
+		n *= 2
+		Deck.set_minimum(100 * n)
+		print(n)
 	Deck.fill_deck_remaining()
 	Deck.draw_hand()
 	$Labels/Energy.set_energy(starting_energy)
@@ -46,5 +48,6 @@ func game_over():
 	print("GAME OVER")
 	Equip.wipe_equipment()
 	Score.reset_money()
+	n = 1 
 	#change to game over screen eventually
 	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
