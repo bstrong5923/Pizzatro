2e610189377116b68f0f2c0537c22690dea62a26
imoore7128 <isaacmoore3325@gmail.com>
2026-04-07 14:05:34 -0700

    fixed button thingy it was slightly broke

--- Scripts/remove_card_prompter.gd
+++ Scripts/remove_card_prompter.gd
@@ -1,7 +1,7 @@
 extends Sprite2D
 
 const REMOVE_BUTTON_PRESSED_TEXTURE := preload("res://Assets/Sprites/game scene items/remove_button_pressed.png")
-
+const REMOVE_BUTTON_TEXTURE := preload("res://Assets/Sprites/game scene items/remove_button.png")
 
 func _on_area_2d_input_event(_viewport, event, _shape_idx):
 	if (event is InputEventMouseButton \
@@ -9,5 +9,5 @@ func _on_area_2d_input_event(_viewport, event, _shape_idx):
 	and event.pressed):
 		texture = REMOVE_BUTTON_PRESSED_TEXTURE
 		await get_tree().create_timer(0.5).timeout
-		texture = null
+		texture = REMOVE_BUTTON_TEXTURE
 		get_node("/root/Game/RemoveCardMenu").open()
