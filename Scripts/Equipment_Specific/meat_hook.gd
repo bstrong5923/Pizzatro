f5596cf4b492a04597d169e48b456feb3a3dae4a
bstrong5923 <158350091+bstrong5923@users.noreply.github.com>
2026-05-07 12:54:25 -0700

    Create meat_hook.gd

---
+++ Scripts/Equipment_Specific/meat_hook.gd
@@ -0,0 +1,17 @@
+extends Equipment
+class_name Meat_Hook
+
+var multiplier = 4.0
+var done = false
+
+func on_card_played(ingredient):
+  if !done and ingredient.types.has("meat"):
+    for f in CardFunction.flavors_to_add.size():
+			CardFunction.flavors_to_add[f] *= multiplier
+    done = true
+
+func on_submit():
+  done = false
+
+func upgrade():
+	multiplier += 0.5
