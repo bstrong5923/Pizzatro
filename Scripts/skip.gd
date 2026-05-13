03bf25da30527424325c4f241089bb653bcbfc97
bstrong5923 <bstrong5923@student.pps.net>
2026-05-13 15:33:53 -0700

    packs look fire now

--- Scripts/skip.gd
+++ Scripts/skip.gd
@@ -1,7 +1,6 @@
 extends Sprite2D
 
-
-
+	
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		Deck.clear_shop_hand()
