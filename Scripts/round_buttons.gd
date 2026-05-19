f9251efc2a1a490d66d5043e2f776f7023b30e8c
LucianLandey <llandey8161@student.pps.net>
2026-05-19 14:22:50 -0700

    updating watermelon and durian. added screenshake with watermelon/durian

--- Scripts/round_buttons.gd
+++ Scripts/round_buttons.gd
@@ -100,9 +100,6 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 						for eq in equips: # no dupes in shop or equips that are already max level/cant be upgraded
 							if e == eq:
 								again = true
-						print(e.name + " - " + str(again))
-						print(e.upgrade_count)
-						print(e.max_upgrades)
 					equips.push_back(e)
 					if e != null: # if you are out of equipment to buy
 						equip_instance.set_equipment(e)
